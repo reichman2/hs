77480fbe5c8d167f6b10829a5f2be80db2f3ba88
Brian <brianwreich@me.com>
2018-10-09 11:26:12 -0500

    Create TwoTruths.java

---
+++ TwoTruths.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class TwoTruths {
+    public static void main(String[] args) {
+        // Start here!
+        
+        System.out.println("Brian Reich \n");
+        System.out.println("+=========================+");
+        System.out.println("| 1. I live in Wisconson. |");
+        System.out.println("| 2. I have two siblings. |");
+        System.out.println("| 3. I have a dog.        |");
+        System.out.println("+=========================+ \n \n");
+        
+        System.out.println("Which is the lie?");
+        Scanner sc = new Scanner(System.in);
+        
+        
+        
+        
+        // Answer Below
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        if (sc.nextLine().equalsIgnoreCase("1")) {
+            System.out.println("Correct!");
+        } else System.out.println("WRONG!!!");
+    }
+}
